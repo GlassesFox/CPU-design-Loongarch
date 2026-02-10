443dd2431f207baf2bd1d225b274c25797fe7fc1
GlassesFox <2077957201@qq.com>
2026-02-10 17:58:59 +0800

    Until EXP7

---
+++ minicpu_env/soc_verify/rtl/xilinx_ip/clk_pll/clk_pll_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
+// Date        : Mon Jan 12 15:01:23 2026
+// Host        : DELL running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               k:/documents/LoongsonEdu/cdp_ede_local/minicpu_env/soc_verify/rtl/xilinx_ip/clk_pll/clk_pll_stub.v
+// Design      : clk_pll
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tfbg676-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* CORE_GENERATION_INFO = "clk_pll,clk_wiz_v6_0_15_0_0,{component_name=clk_pll,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,enable_axi=0,feedback_source=FDBK_AUTO,PRIMITIVE=PLL,num_out_clk=2,clkin1_period=10.000,clkin2_period=10.000,use_power_down=false,use_reset=false,use_locked=false,use_inclk_stopped=false,feedback_type=SINGLE,CLOCK_MGR_TYPE=NA,manual_override=false}" *) 
+module clk_pll(cpu_clk, timer_clk, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_in1" */
+/* synthesis syn_force_seq_prim="cpu_clk" */
+/* synthesis syn_force_seq_prim="timer_clk" */;
+  output cpu_clk /* synthesis syn_isclock = 1 */;
+  output timer_clk /* synthesis syn_isclock = 1 */;
+  input clk_in1;
+endmodule
